73d54fc9f5212f7ac66e479b581c86c446693236
Khristian Boone <kboone801@gmail.com>
2026-08-26 16:45:30 -0400

    Modified Const Version to "const Version = (typeof GM_info !== 'undefined' && GM_info.script) ? GM_info.script.version : "1.0.0"; "

--- Naughty Inventory Companion.user.js
+++ Naughty Inventory Companion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Naughty Inventory Companion
 // @namespace    https://github.com/SharpSplinter/Naughty-Inventory-Companion
-// @version      1.2.17
+// @version      1.2.18
 // @description  Manual Torn inventory tracker with live market values, equipment perks, mods, and loan status.
 // @author       SharpSplinter [315311]
 // @license      MIT
@@ -27,7 +27,7 @@
 (function () {
     "use strict";
 
-    const VERSION = GM_info.script.version;
+    const Version = (typeof GM_info !== 'undefined' && GM_info.script) ? GM_info.script.version : "1.0.0";
     console.log('Running script version: ${VERSION}');
     
     const BASE_URL = "https://api.torn.com/v2/";
