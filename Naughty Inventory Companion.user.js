398112f4d2fff0659da67346061ac53690735b0b
Khristian Boone <kboone801@gmail.com>
2026-08-26 17:15:27 -0400

    SharpSplinter adjusted and fixed Const VERSION line (Confirmed)

--- Naughty Inventory Companion.user.js
+++ Naughty Inventory Companion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Naughty Inventory Companion
 // @namespace    https://github.com/SharpSplinter/Naughty-Inventory-Companion
-// @version      1.2.17
+// @version      1.2.18
 // @description  Manual Torn inventory tracker with live market values, equipment perks, mods, and loan status.
 // @author       SharpSplinter [315311]
 // @license      MIT
